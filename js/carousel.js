b129ca217cf93b5222abaea4d099c86d091c0a0c
Estella-Lee <krsd0501@gmail.com>
2021-09-20 17:55:06 +0900

    our artists 슬라이드 임시작업

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,32 @@
-const $ = selector => {
-    return document.querySelector(selector);
-};
-
-fuction 
+$(document).ready(function(){
+	$('.artistList').slick({
+	  centerMode: true,
+	  centerPadding: '10px',
+	  infinite: true,
+	  slidesToShow: 5,
+	  autoplay: true,
+	  speed: 1500,
+	  index: 1,
+	  focusOnSelect: true
+//  responsive: [
+//    {
+//      breakpoint: 768,
+//      settings: {
+//        arrows: false,
+//        centerMode: true,
+//        centerPadding: '40px',
+//        slidesToShow: 3
+//      }
+//    },
+//    {
+//      breakpoint: 480,
+//      settings: {
+//        arrows: false,
+//        centerMode: true,
+//        centerPadding: '40px',
+//        slidesToShow: 1
+//      }
+//    }
+//  ]
+});
+   });
