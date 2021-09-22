9de5b706bdde15f1d362e6c0b3725dbb4cfd2210
Estella-Lee <krsd0501@gmail.com>
2021-09-22 21:52:11 +0900

    main video 태그 변경 및 효과 적용

--- js/carousel.js
+++ js/carousel.js
@@ -12,21 +12,21 @@ $(document).ready(function(){
 //    {
 //      breakpoint: 768,
 //      settings: {
-//        arrows: false,
-//        centerMode: true,
-//        centerPadding: '40px',
+//     //    arrows: false,
+//     //    centerMode: true,
+//     //    centerPadding: '40px',
 //        slidesToShow: 3
 //      }
 //    },
 //    {
 //      breakpoint: 480,
 //      settings: {
-//        arrows: false,
-//        centerMode: true,
-//        centerPadding: '40px',
+//     //    arrows: false,
+//     //    centerMode: true,
+//     //    centerPadding: '40px',
 //        slidesToShow: 1
 //      }
 //    }
 //  ]
+	});
 });
-   });
