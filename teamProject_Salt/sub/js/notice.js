ed27a7239c5bdcd1dbf15b5cb5869bd5df912ba2
srcre05 <82811108+srcre05@users.noreply.github.com>
2021-09-30 16:53:17 +0900

    함수실행 수정

--- teamProject_Salt/sub/js/notice.js
+++ teamProject_Salt/sub/js/notice.js
@@ -16,16 +16,10 @@ const prevNoticeTitle = document.querySelectorAll('.nextPrevBtn p:last-child');
 
 
 // 뉴스 슬라이드
-newsSlideBtn[0].onclick = function() { slide(0) };
-newsSlideBtn[1].onclick = function() { slide(1) };
-newsSlideBtn[2].onclick = function() { slide(2) };
-newsSlideBtn[3].onclick = function() { slide(3) };
-newsSlideBtn[4].onclick = function() { slide(4) };
-newsSlideBtn[5].onclick = function() { slide(5) };
-newsSlideBtn[6].onclick = function() { slide(6) };
-newsSlideBtn[7].onclick = function() { slide(7) };
-newsSlideBtn[8].onclick = function() { slide(8) };
-newsSlideBtn[9].onclick = function() { slide(9) };
+
+for (let i=0; i<newsSlideBtn.length; i++) {
+    newsSlideBtn[i].onclick = function() { slide(i) };
+}
 
 
 function slide(i) {
@@ -40,27 +34,15 @@ function slide(i) {
 
 
 // 전체 기사 & 공지
-more[0].onclick = function() { moreArticle(0) };
-more[1].onclick = function() { moreArticle(1) };
-more[2].onclick = function() { moreArticle(2) };
-more[3].onclick = function() { moreArticle(3) };
-more[4].onclick = function() { moreArticle(4) };
-more[5].onclick = function() { moreArticle(5) };
-more[6].onclick = function() { moreArticle(6) };
-more[7].onclick = function() { moreArticle(7) };
-more[8].onclick = function() { moreArticle(8) };
-more[9].onclick = function() { moreArticle(9) };
+for (let i=0; i<more.length; i++) {
+    more[i].onclick = function() { moreArticle(i) };
+    articleCloseBtn[i].onclick = function() { closeArticle(i) };
+}
 
-noticeList[0].onclick = function() { moreNotice(0) };
-noticeList[1].onclick = function() { moreNotice(1) };
-noticeList[2].onclick = function() { moreNotice(2) };
-noticeList[3].onclick = function() { moreNotice(3) };
-noticeList[4].onclick = function() { moreNotice(4) };
-noticeList[5].onclick = function() { moreNotice(5) };
-noticeList[6].onclick = function() { moreNotice(6) };
-noticeList[7].onclick = function() { moreNotice(7) };
-noticeList[8].onclick = function() { moreNotice(8) };
-noticeList[9].onclick = function() { moreNotice(9) };
+for (let i=0; i<noticeList.length; i++) {
+    noticeList[i].onclick = function() { moreNotice(i) };
+    noticeCloseBtn[i].onclick = function() { closeArticle(i) };
+}
 
 function moreArticle(i) {
     articleFullText[i].style.transform = 'translateY(0%)';
@@ -74,28 +56,6 @@ function moreNotice(i) {
     document.body.style.overflowY = 'hidden';
 }
 
-articleCloseBtn[0].onclick = function() { closeArticle(0) };
-articleCloseBtn[1].onclick = function() { closeArticle(1) };
-articleCloseBtn[2].onclick = function() { closeArticle(2) };
-articleCloseBtn[3].onclick = function() { closeArticle(3) };
-articleCloseBtn[4].onclick = function() { closeArticle(4) };
-articleCloseBtn[5].onclick = function() { closeArticle(5) };
-articleCloseBtn[6].onclick = function() { closeArticle(6) };
-articleCloseBtn[7].onclick = function() { closeArticle(7) };
-articleCloseBtn[8].onclick = function() { closeArticle(8) };
-articleCloseBtn[9].onclick = function() { closeArticle(9) };
-
-noticeCloseBtn[0].onclick = function() { closeArticle(0) };
-noticeCloseBtn[1].onclick = function() { closeArticle(1) };
-noticeCloseBtn[2].onclick = function() { closeArticle(2) };
-noticeCloseBtn[3].onclick = function() { closeArticle(3) };
-noticeCloseBtn[4].onclick = function() { closeArticle(4) };
-noticeCloseBtn[5].onclick = function() { closeArticle(5) };
-noticeCloseBtn[6].onclick = function() { closeArticle(6) };
-noticeCloseBtn[7].onclick = function() { closeArticle(7) };
-noticeCloseBtn[8].onclick = function() { closeArticle(8) };
-noticeCloseBtn[9].onclick = function() { closeArticle(9) };
-
 function closeArticle(i) {
     articleFullText[i].style.transform = 'translateY(100%)';
     articleFullText[i].style.overflowY = 'hidden';
@@ -108,6 +68,10 @@ function closeArticle(i) {
 
 
 // 공지 이전글 다음글
+for (let i=0; i<noticeList.length; i++) {
+    plusPrevNext(i);
+}
+
 function plusPrevNext(i) {
     if (i > 0) {
         const nextTitle = document.createElement('a');
@@ -135,17 +99,6 @@ function plusPrevNext(i) {
     }
 }
 
-plusPrevNext(0);
-plusPrevNext(1);
-plusPrevNext(2);
-plusPrevNext(3);
-plusPrevNext(4);
-plusPrevNext(5);
-plusPrevNext(6);
-plusPrevNext(7);
-plusPrevNext(8);
-plusPrevNext(9);
-
 
 // more
 const moreBtn = document.querySelector('.more span');
@@ -154,6 +107,8 @@ const noticeAllList = document.querySelector('#noticeAllList');
 moreBtn.onclick = () => {
     noticeAllList.style.height = '1500px'
 
+    listChange(6);
+
     function listChange(j) {
         for (let i=j; i < noticeList.length; i++) {
             noticeList[i].classList.add('listPop');
@@ -161,8 +116,6 @@ moreBtn.onclick = () => {
 
         moreBtn.innerHTML = 'End';
     }
-
-    listChange(6);
 
     if (window.matchMedia("(max-width: 1024px)").matches) {
         noticeAllList.style.height = '1350px'
