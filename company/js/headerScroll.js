8de236e66bf8ea62d2cb1abeda433cc8a1fd5304
hyunkyung <49914580+wsay0322@users.noreply.github.com>
2021-10-11 12:25:11 +0900

    commit
    최종

--- company/js/headerScroll.js
+++ company/js/headerScroll.js
@@ -1,49 +1,55 @@
 $(window).on('load', function() {
 
-  $(".logoMenu").css("width", "100%");
-  let logoMenu2 = $(".logoMenu").clone().addClass("logoMenu2").appendTo("header");
-  let lastScroll = 300;
+    $(".logoMenu").css("width", "100%");
+    let logoMenu2 = $(".logoMenu").clone().addClass("logoMenu2").appendTo("header");
+    let lastScroll = 300;
 
-  $(".logoMenu2").css({
-      "position" : "fixed",
-      "top" : "0",
-      "opacity" : "0",
-      "zIndex" : "999999",
-      "transition" : "all 0.3s"
-  })
+    $(logoMenu2).css({
+        "position" : "fixed",
+        "top" : "0",
+        "visibility" : "hidden",
+        "zIndex" : "999",
+        "transition" : "all 0.3s"
+    })
 
-  $(window).scroll(function(){
-      let scrollMove = $(window).scrollTop();
+    $(window).scroll(function(){
+        let scrollMove = $(window).scrollTop();
 
-      if (scrollMove == 0) {
-          $(logoMenu2).css("opacity", "0");
+        if (scrollMove == 0) {
+            $(logoMenu2).css({
+                "opacity" : "0",
+                "visibility" : "hidden"
+            });
 
-      } else if (scrollMove > lastScroll) {
-          $(".logoMenu2").css({
-              "top" : "-20%",
-              "backgroundColor" : "#fff"
-          });
+        } else if (scrollMove > lastScroll) {
+            $(logoMenu2).css({
+                "top" : "-20%",
+                "backgroundColor" : "#fff"
+            });
 
-      } else {
-          $(".logoMenu2").css({
-              "opacity" : "1",
-              "top" : "0",
-          });
+        } else {
+            $(logoMenu2).css({
+                "opacity" : "1",
+                "visibility" : "visible",
+                "top" : "0"
+            });
+    
+            $(".logoMenu2 .gnb li>a").css("color", "#000");
+            $(".logoMenu2 .loginSignup li>a").css("color", "#000");
+            $(".logoMenu2 .logo h1 img").attr("src", "./images/logo.png");
+        } 
+        lastScroll = scrollMove;
+    });
 
-          $(".logoMenu2 .gnb li>a").css("color", "#000");
-          $(".logoMenu2 .loginSignup li>a").css("color", "#000");
-          $(".logoMenu2 .logo h1 img").attr("src", "./images/logo.png");
-      } 
-      lastScroll = scrollMove;
-  });
+    $(window).resize(function () {
+        if (window.matchMedia("(max-width:768px)").matches) {
+            $(".logoMenu2").css("display", "none");
+        } else {
+            $(".logoMenu2").css("display", "block");
+        }
+    });
 
     if (window.matchMedia("(max-width:768px)").matches) {
-        $(".logoMenu2").hide();
+        $(".logoMenu2").css("display", "none");
     }
-
-  $(window).resize(function () {
-    if (window.matchMedia("(max-width:768px)").matches) {
-        $(".logoMenu2").hide();
-    }
-  });
 });
